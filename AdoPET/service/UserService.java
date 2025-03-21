c24b356921c12f0e727d10b1fcce43bf9584b539
Ana Ripoli <anaripoli.eng@gmail.com>
2025-03-20 21:04:41 -0300

    criação de userService para verificação de Email e Senha

--- AdoPET/service/UserService.java
+++ AdoPET/service/UserService.java
@@ -1,5 +1,20 @@
 package AdoPET.service;
 
+import AdoPET.dao.PseudoDataBase;
+import AdoPET.model.User;
+
 public class UserService {
 
+    //Verificação para saber se o email está de acordo com a regra (conter "@")
+    public User verificacaoEmail(String email) {
+        User doesUserExist = PseudoDataBase.doesUserExist(email);
+        if(doesUserExist == null && email.contains("@") == false) {
+            return null;
+        }
+        return doesUserExist;
+    }
+
+    public boolean verificacaoSenha(User userAlvo, String password) {
+        return userAlvo.getPassword().equals(password);
+    }
 }
