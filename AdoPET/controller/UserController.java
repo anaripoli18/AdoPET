e58694d507b08d0d1bbfdf6a3ba666b903b88cd3
Ana Ripoli <anaripoli.eng@gmail.com>
2025-03-24 21:15:41 -0300

    construtor criado em UserController

--- AdoPET/controller/UserController.java
+++ AdoPET/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import AdoPET.dao.PseudoDataBase;
 import AdoPET.model.User;
 import AdoPET.service.UserService;
+import AdoPET.view.LoginText;
+import AdoPET.view.Profile;
 
 public class UserController implements Controller {
     
@@ -13,4 +15,17 @@ public class UserController implements Controller {
     UserService userService;
     Scanner scanner;
     int escolhaDeUsuario;
+
+    public UserController() {
+        loginText = new LoginText();
+        profileScreen = new Profile();
+        userService = new UserService();
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void abrirView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'abrirView'");
+    }
 }
